6618c265a9cbe84b88425a779eaee5a7970bbd85
Andy Kearney <akearney@hmc.edu>
2018-07-18 21:29:20 -0700

    add events to calendar

--- node/src/components/calendar.js
+++ node/src/components/calendar.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 import calendar from '../data/calendar.json';
+import logs from '../data/session_logs.json';
 import './calendar.css';
 
+Modal.setAppElement('#root');
+
 export default class Calendar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      modalOpen: false,
+      currentLog: ''
+    };
+  }
+
+  handleClick(e) {
+    const month = e.target.getAttribute('month');
+    const day = e.target.getAttribute('day');
+    if (month && day && logs[month][day]) {
+      this.setState({
+        modalOpen: true,
+        currentMonth: month,
+        currentDay: day,
+        currentLog: logs[month][day]
+      });
+    }
+  }
+
+  closeModal() {
+    this.setState({ modalOpen: false });
+  }
+
   render() {
     const months = [];
     const numDaysInWeek = calendar['weekdays'].length;
@@ -17,12 +46,16 @@ export default class Calendar extends Component {
 
       let currentWeek = [];
       for (let k = 0; k < month['days']; k++) {
+        let dayClass = 'day';
         if (calendar['current']['month'] === month['name'] &&
             calendar['current']['day'] === k + 1) {
-          currentWeek.push(<td title="Today" className="day today" key={k}>{ k + 1 }</td>);
-        } else {
-          currentWeek.push(<td className="day" key={k}>{ k + 1 }</td>);
+          dayClass += ' today';
         }
+        if (logs[month['name']] && logs[month['name']][k + 1 + '']) {
+          dayClass += ' hasLog';
+        }
+        currentWeek.push(<td month={ month['name'] } day={ k + 1 }
+                         className={ dayClass } key={k}>{ k + 1 }</td>);
         if ((k + 1) % numDaysInWeek === 0) {
           if (currentWeek.length) {
             const key = `week${ k / numDaysInWeek }`;
@@ -52,8 +85,19 @@ export default class Calendar extends Component {
     }
 
     return (
-      <div className="calendar">
-        { months }
+      <div>
+        <div className="calendar" onClick={ this.handleClick.bind(this) }>
+          { months }
+        </div>
+        <Modal isOpen={ this.state.modalOpen }
+            onRequestClose={ this.closeModal.bind(this) }>
+          <div className="logContent">
+            <h3>{ this.state.currentMonth } { this.state.currentDay }</h3>
+            <button className="close" onClick={ this.closeModal.bind(this) }>X</button>
+            <div className="log" dangerouslySetInnerHTML={{ __html: this.state.currentLog }}>
+            </div>
+          </div>
+        </Modal>
       </div>
     );
   }
