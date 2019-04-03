36da5bdfaea0086a0c7e22ef86282e3b9e60347a
Andy Kearney <akearney@fb.com>
2019-04-03 15:09:35 -0700

    push script

--- node/push.sh
+++ node/push.sh
@@ -1,6 +1,8 @@
 npm run build
 local_path=build
 remote_path=/home/protected/andynd/node
-sftp -v -oIdentityFile=password.txt kearneyandy_andynd@ssh.phx.nearlyfreespeech.net <<EOF
-put -r $local_path $remote_path
-EOF
+rsync -av --exclude tiles/* build kearneyandy_andynd@ssh.phx.nearlyfreespeech.net:/home/protected/andynd/node
+
+# sftp -v -oIdentityFile=password.txt kearneyandy_andynd@ssh.phx.nearlyfreespeech.net <<EOF
+# put -r $local_path $remote_path
+# EOF
